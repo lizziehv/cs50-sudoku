fad3ea21786ffdb2f35cb4471cdf80e52608f8fa
Maria P. Mora Bolanos 22 <mbolanos@moose.cs.dartmouth.edu>
2020-05-26 20:44:49 -0400

    first commit

---
+++ common/common.c
@@ -0,0 +1,32 @@
+/* 
+ * common.c 
+ * see IMPLEMENTATION.md and DESIGN.md for more information.
+ *
+ * Maria Paula Mora, Lizzie Hernandez, Ethan Trepka, and Kshitij Jain 
+ * May 2020
+ * Purpose: print a sudoku into stdout
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// prints the sudoku grid
+void print_sudoku(int** Sudoku) {
+    
+    // for every row
+    for (int i = 0; i < 9; i++) {
+        // for every column
+        for (int j = 0; j < 9; j++) {
+            printf("%d ", Sudoku[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// checks to see if you can add a specific entry and still have a valid grid
+bool check_entry(int** Sudoku, int row, int column, int entry) {
+    // NEEDS TO BE FILLED
+    return true;
+}
---
+++ solve/solve.c
@@ -0,0 +1,20 @@
+/* 
+ * solve.c 
+ * see IMPLEMENTATION.md and DESIGN.md for more information.
+ *
+ * Maria Paula Mora, Lizzie Hernandez, Ethan Trepka, and Kshitij Jain 
+ * May 2020
+ * Purpose: solve a given sudoku
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**************** Global functions ****************/
+
+/* solves a given sudoku
+* if there is no solution, returns NULL */
+int** solve(int** sudoku) {
+
+}
